a972ce703a6b71ec7bf7d09a697aa1c679a484d9
mdshakilkhan841 <mdshakilkhan841@gmail.com>
2023-03-09 04:34:08 +0600

    Updated

--- src/Components/Image.js
+++ src/Components/Image.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
-const Image = () => {
+const Image = ({ formData, setFormData }) => {
+
     return (
         <div>
             <div className='flex justify-between font-semibold text-2xl mb-6'>
@@ -9,11 +10,15 @@ const Image = () => {
             </div>
             <div className="mb-6">
                 <label for="file" className="block mb-2 text-md font-medium text-gray-900 dark:text-white text-left">Upload Your Photo: <span className='text-rose-600'>*</span></label>
-                <input type="file" id="file" className="bg-gray-50 border border-gray-300 text-gray-900 text-md rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Email Id" required />
+                <input type="file" id="file" className="bg-gray-50 border border-gray-300 text-gray-900 text-md rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Email Id" required
+                    onChange={(e) => setFormData({ ...formData, image: e.target.files[0] })}
+                />
             </div>
             <div className="mb-6">
                 <label for="file" className="block mb-2 text-md font-medium text-gray-900 dark:text-white text-left">Upload Signature Photo: <span className='text-rose-600'>*</span></label>
-                <input type="file" id="file" className="bg-gray-50 border border-gray-300 text-gray-900 text-md rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Email Id" required />
+                <input type="file" id="file" className="bg-gray-50 border border-gray-300 text-gray-900 text-md rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Email Id" required
+                    onChange={(e) => setFormData({ ...formData, signature: e.target.files[0] })}
+                />
             </div>
         </div>
     )
